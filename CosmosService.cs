2b40aaf7e8e1ab0a00217900ce9d4718dda377cb
Pablo Rodriguez <pablo.rodriguez.musre@gmail.com>
2025-09-26 00:12:32 -0300

    add namespace to CosmosService

--- CosmosService.cs
+++ CosmosService.cs
@@ -1,19 +1,21 @@
 ﻿using Microsoft.Azure.Cosmos;
-using Pokemonfn;
 
-public class CosmosService
+namespace Pokemonfn
 {
-    private readonly Container _container;
-
-    public CosmosService(string connectionString, string databaseId, string containerId)
+    public class CosmosService
     {
-        var client = new CosmosClient(connectionString);
-        _container = client.GetContainer(databaseId, containerId);
-    }
+        private readonly Container _container;
 
-    public async Task InsertAsync(Pokemon pkmn)
-    {
-        var response = await _container.CreateItemAsync(pkmn, new PartitionKey(pkmn.PartitionKey));
-        Console.WriteLine($"Status: {response.StatusCode}, ActivityId: {response.ActivityId}");
+        public CosmosService(string connectionString, string databaseId, string containerId)
+        {
+            var client = new CosmosClient(connectionString);
+            _container = client.GetContainer(databaseId, containerId);
+        }
+
+        public async Task InsertAsync(Pokemon pkmn)
+        {
+            var response = await _container.CreateItemAsync(pkmn, new PartitionKey(pkmn.PartitionKey));
+            Console.WriteLine($"Status: {response.StatusCode}, ActivityId: {response.ActivityId}");
+        }
     }
 }
