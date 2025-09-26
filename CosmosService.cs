c41ca96b5fa79c44c8a7c72582e093ba634a3a39
Pablo Rodriguez <pablo.rodriguez.musre@gmail.com>
2025-09-26 00:11:35 -0300

    Agregar archivos de proyecto.

---
+++ CosmosService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Cosmos;
+using Pokemonfn;
+
+public class CosmosService
+{
+    private readonly Container _container;
+
+    public CosmosService(string connectionString, string databaseId, string containerId)
+    {
+        var client = new CosmosClient(connectionString);
+        _container = client.GetContainer(databaseId, containerId);
+    }
+
+    public async Task InsertAsync(Pokemon pkmn)
+    {
+        var response = await _container.CreateItemAsync(pkmn, new PartitionKey(pkmn.PartitionKey));
+        Console.WriteLine($"Status: {response.StatusCode}, ActivityId: {response.ActivityId}");
+    }
+}
---
+++ Pokemonfn.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Pokemonfn
+{
+    public class Pokemonfn
+    {
+        [Function("Pokemonfn")]
+        [CosmosDBOutput("az204", "pokemon", Connection = "CosmosDbConnection")]
+        public static Pokemon Run(
+            [QueueTrigger("pokemon", Connection = "AzureWebJobsStorage")] QueueMessage msg,
+            FunctionContext ctx
+        )
+        {
+            var logger = ctx.GetLogger("pkmn");
+
+            try
+            {
+                var pkmn = msg.Body.ToObjectFromJson<Pokemon>();
+                if (pkmn is null)
+                {
+                    throw new Exception("Invalid Pokemon object.");
+                }
+
+                logger.LogInformation($"Inserting Pokémon: {JsonSerializer.Serialize(pkmn)}");
+
+                return pkmn;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to process message: {ex.Message}");
+                throw;
+            }
+        }
+
+    }
+
+    public class Pokemon
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [JsonPropertyName("nmb")]
+        public int Nmb { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+
+        [JsonPropertyName("partitionKey")]
+        public string PartitionKey { get; set; } = "pokemon";
+    }
+}
